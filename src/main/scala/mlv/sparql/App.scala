c82dd9eddc644cd38c16b0f5c0478f55744e225e
Zacharie BARBECOT <z.barbecot@gmail.com>
2016-04-10 18:12:54 +0200

    Test dsl

--- src/main/scala/mlv/sparql/App.scala
+++ src/main/scala/mlv/sparql/App.scala
@@ -5,7 +5,6 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.jena.query.QueryFactory
 import org.apache.jena.sparql.algebra.Algebra
-
 object App {
 
   val FILE_UNIV = "data/univ1.nt"
@@ -13,6 +12,8 @@ object App {
   val FILE_CONCEPTS = "data/univConcepts.txt"
   val FILE_DICT = "data/univ1_dict.txt"
   val FILE_ENCODED = "data/univ1_encoded.txt"
+  val FILE_ALGEBRA = "data/algebra.txt"
+  val FILE_RESULT = "data/result.txt"
 
   case class Univ(s1: String, s2: String, s3: String)
   case class Props(l1: Long, s1: String)
@@ -21,6 +22,8 @@ object App {
   case class Encoded(l1: Long, l2: Long, l3: Long)
 
   def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "C:\\Users\\zacha\\Documents\\workspace\\sparql\\winutil\\")
+
     val conf = new SparkConf()
       .setAppName("Sparql project")
       .setMaster("local")
@@ -30,15 +33,10 @@ object App {
     import sqlContext.implicits._
 
     val univ = sc.textFile(FILE_UNIV).map(_.split(" ")).map(u => Univ(u(0), u(1), u(2))).toDF
-    univ.registerTempTable("univ")
     val props = sc.textFile(FILE_PROPS).map(_.split(" ")).map(p => Props(p(0).trim.toLong, p(1))).toDF
-    props.registerTempTable("props")
     val concepts = sc.textFile(FILE_CONCEPTS).map(_.split(" ")).map(c => Concepts(c(0).trim.toLong, c(1))).toDF
-    concepts.registerTempTable("concepts")
     val dict = sc.textFile(FILE_DICT).map(_.split(" ")).map(d => Dict(d(0), d(1).trim.toLong)).toDF
-    dict.registerTempTable("dict")
     val encoded = sc.textFile(FILE_ENCODED).map(_.split(" ")).map(e => Encoded(e(0).trim.toLong, e(1).trim.toLong, e(2).trim.toLong)).toDF
-    encoded.registerTempTable("encoded")
 
     val qstr = """PREFIX  lubm: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
         PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -51,7 +49,22 @@ object App {
           }"""
     val query = QueryFactory.create(qstr)
     val op = Algebra.compile(query)
-    println(op)
+    scala.tools.nsc.io.File(FILE_ALGEBRA).writeAll(op.toString())
+    val algebra = sc.textFile(FILE_ALGEBRA).map(_.split("\n"))
+
+    //Utilisation des dsl dataframes
+    /*
+    val u1 = univ.select("*").withColumnRenamed("s1", "s").withColumnRenamed("s2", "p").withColumnRenamed("s3", "o")
+    val s1 = dict.select("*").withColumnRenamed("s1", "s")
+    val p1 = props.select("*").withColumnRenamed("s1", "p").withColumnRenamed("l1", "l2")
+    val c1 = concepts.select("*").withColumnRenamed("s1", "o").withColumnRenamed("l1", "l3")
+
+    val res = u1.join(s1, Seq("s")).select("l1", "p", "o")
+    val res1 = res.join(p1, Seq("p")).select("l1", "l2", "o")
+    val res2 = res1.join(c1, Seq("o")).select("l1", "l2", "l3")
+    val res3 = res2.unionAll(encoded)
+    res3.rdd.coalesce(1, true).saveAsTextFile(FILE_RESULT)
+		*/
 
     sc.stop
   }
